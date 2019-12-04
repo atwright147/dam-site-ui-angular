e3b6e881177df0e7378eeb8e0a9c22514382f05e
Andy Wright <atwright147@gmail.com>
2019-12-04 23:42:40 +0000

    implement a nested reactive form thumbnail component

--- src/app/components/thumbnail/thumbnail.component.ts
+++ src/app/components/thumbnail/thumbnail.component.ts
@@ -1,20 +1,69 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { ControlContainer, NgForm } from '@angular/forms';
+import { Component, Input, forwardRef, HostBinding } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-thumbnail',
   templateUrl: './thumbnail.component.html',
   styleUrls: ['./thumbnail.component.scss'],
-  viewProviders: [ { provide: ControlContainer, useExisting: NgForm } ]
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => ThumbnailComponent),
+      multi: true
+    }
+  ]
 })
-export class ThumbnailComponent implements OnInit {
+export class ThumbnailComponent implements ControlValueAccessor {
+  @HostBinding('attr.id')
+  externalId = '';
+
+  @Input()
+  set id(value: string) {
+    this._ID = value;
+    this.externalId = null;
+  }
+
   @Input() imageUrl: string;
   @Input() imageId: string;
   @Input() index: number;
 
-  constructor() { }
-
-  ngOnInit() {
+  get id() {
+    return this._ID;
   }
 
+  private _ID = '';
+
+  @Input('value') _value = false;
+  onChange: any = () => {};
+  onTouched: any = () => {};
+
+  get value() {
+    return this._value;
+  }
+
+  set value(val) {
+    this._value = val;
+    this.onChange(val);
+    this.onTouched();
+  }
+
+  constructor() {}
+
+  registerOnChange(fn) {
+    this.onChange = fn;
+  }
+
+  writeValue(value) {
+    if (value) {
+      this.value = value;
+    }
+  }
+
+  registerOnTouched(fn) {
+    this.onTouched = fn;
+  }
+
+  switch() {
+    this.value = !this.value;
+  }
 }
