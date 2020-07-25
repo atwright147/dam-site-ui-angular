662c12e8b7b86493d4e46b6d46ceb01977206b0b
Andy Wright <atwright147@gmail.com>
2020-07-25 22:47:38 +0100

    download stub images

---
+++ stubs/download.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+rm -f images/*
+
+wget "https://picsum.photos/750/1000?random=1" -O ./images/image-1.jpg
+wget "https://picsum.photos/750/1000?random=1" -O ./images/image-2.jpg
+wget "https://picsum.photos/750/1000?random=1" -O ./images/image-3.jpg
+wget "https://picsum.photos/750/1000?random=1" -O ./images/image-4.jpg
+wget "https://picsum.photos/750/1000?random=1" -O ./images/image-5.jpg
+wget "https://picsum.photos/750/1000?random=1" -O ./images/image-6.jpg
+wget "https://picsum.photos/750/1000?random=1" -O ./images/image-7.jpg
+wget "https://picsum.photos/750/1000?random=1" -O ./images/image-8.jpg
+wget "https://picsum.photos/750/1000?random=1" -O ./images/image-9.jpg
+wget "https://picsum.photos/750/1000?random=1" -O ./images/image-10.jpg
