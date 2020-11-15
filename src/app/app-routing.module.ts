3a0ed275c4893c504f078e78a80a80cb31c0ac6e
Andy Wright <atwright147@gmail.com>
2020-11-15 18:36:36 +0000

    add routes and buttons to allow switching layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { FilmstripComponent } from './components/layout/filmstrip/filmstrip.component';
+import { GridComponent } from './components/layout/grid/grid.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'layout/filmstrip', component: FilmstripComponent },
+  { path: 'layout/grid', component: GridComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
