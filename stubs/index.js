3ef4db9f8224a0b3f846ed7d4ce82b30f59b2fee
Andy Wright <atwright147@gmail.com>
2020-11-17 22:41:47 +0000

    remove deprecated body-parser package

--- stubs/index.js
+++ stubs/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 const PORT = 3000;
 
