e7c0abe7d9f70a695886f75083960f906cf800ce
Andy Wright <atwright147@gmail.com>
2021-03-21 11:21:47 +0000

    get current cell index in pressing Down Arrow

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -4,6 +4,7 @@ import {
   Component,
   ElementRef,
   HostListener,
+  OnDestroy,
   OnInit,
   QueryList,
   ViewChild,
@@ -11,7 +12,10 @@ import {
 } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { NgResizeObserver, ngResizeObserverProviders } from 'ng-resize-observer';
+import { Subscription } from 'rxjs';
+
 import { MediaService } from '../../services/media.service';
+import { IFile } from '../../interfaces/files.interface';
 
 @Component({
   selector: 'app-carousel',
@@ -20,12 +24,12 @@ import { MediaService } from '../../services/media.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [...ngResizeObserverProviders],
 })
-export class CarouselComponent implements AfterViewInit, OnInit {
+export class CarouselComponent implements AfterViewInit, OnDestroy, OnInit {
   @ViewChild('carousel') carousel: ElementRef<HTMLElement>;
   @ViewChildren('cellContainer', { emitDistinctChangesOnly: true }) cells: QueryList<ElementRef<HTMLElement>>;
   height$ = this.resize$.pipe(map(entry => entry.contentRect.height));
   width$ = this.resize$.pipe(map(entry => entry.contentRect.width));
-  selection$ = this.mediaService.selected$;
+  selected: IFile[];
 
   cellCount: number;
   cellHeight: number;
@@ -33,34 +37,63 @@ export class CarouselComponent implements AfterViewInit, OnInit {
   isHorizontal = true;
   radius: number;
   rotateFn = this.isHorizontal ? 'rotateY' : 'rotateX';
-  selectedIndex = 0;
+  rotationIndex = 0;
   theta: number;
+
+  private _selectionIndex = 0;
+  private readonly subs: Subscription[] = [];
+
+  set selectionIndex(value: number) {
+    if (value > this.selected.length) {
+      this._selectionIndex = value / this.selected.length;
+    } else if (value < 0) {
+      this._selectionIndex = Math.abs(value) / this.selected.length;
+    } else {
+      this._selectionIndex = value;
+    }
+  }
+
+  get selectionIndex() {
+    return this._selectionIndex;
+  }
 
   constructor(
     private readonly mediaService: MediaService,
     private readonly resize$: NgResizeObserver,
-    ) {}
+  ) {}
 
   @HostListener('window:keyup', ['$event'])
   onKeyup(event: KeyboardEvent): void {
     switch (event.key) {
       case 'ArrowLeft':
-        this.selectedIndex--;
+        this.rotationIndex--;
+        this.selectionIndex = this.rotationIndex;
         this.rotate();
         break;
 
       case 'ArrowRight':
-        this.selectedIndex++;
+        this.rotationIndex++;
+        this.selectionIndex = this.rotationIndex;
         this.rotate();
         break;
 
       case 'ArrowDown':
-        console.info(this.selectedIndex);
+        console.info('cellIndex', this.getCellIndex(this.rotationIndex, this.selected.length));
         break;
     }
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    const selectedSub = this.mediaService.selected$.subscribe(
+      (data) => this.selected = data,
+    );
+
+    this.subs.push(selectedSub);
+  }
+
+  ngOnDestroy() {
+    this.subs.forEach((sub) => sub.unsubscribe());
+  }
 
   ngAfterViewInit(): void {
     const cellRect = this.cells.first.nativeElement.getBoundingClientRect();
@@ -73,12 +106,12 @@ export class CarouselComponent implements AfterViewInit, OnInit {
   }
 
   rotate(): void {
-    const angle = this.theta * this.selectedIndex * -1;
+    const angle = this.theta * this.rotationIndex * -1;
     this.carousel.nativeElement.style.transform = this.rotateFn + '(' + angle + 'deg)';
 
     const cellInner = this.carousel.nativeElement.querySelectorAll('.carousel__cell');
     cellInner.forEach((item: HTMLDivElement, index) => {
-      const itemAngle = (this.selectedIndex - index++) * this.theta;
+      const itemAngle = (this.rotationIndex - index++) * this.theta;
       item.style.transform = this.rotateFn + '(' + itemAngle + 'deg)';
     });
   }
@@ -110,5 +143,11 @@ export class CarouselComponent implements AfterViewInit, OnInit {
     this.isHorizontal = true;
     this.rotateFn = this.isHorizontal ? 'rotateY' : 'rotateX';
     this.change();
+  }
+
+  private getCellIndex(rotationIndex: number, cellQuantity: number): number {
+    const remainder = Math.floor(Math.abs(rotationIndex / (cellQuantity)));
+    const remainderMultiple = remainder * cellQuantity;
+    return (rotationIndex - remainderMultiple);
   }
 }
