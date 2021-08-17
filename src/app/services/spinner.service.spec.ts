819ed2e7bf044240af869b8290c243f519e4ed5c
Andy Wright <atwright147@gmail.com>
2021-08-17 22:10:38 +0100

    add tests for spinner service

--- src/app/services/spinner.service.spec.ts
+++ src/app/services/spinner.service.spec.ts
@@ -13,4 +13,20 @@ describe('SpinnerService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('add()', () => {
+    it('should add 1', () => {
+      service.add();
+      service.counter$.subscribe((value) => expect(value).toEqual(1));
+    });
+  });
+
+  describe('remove()', () => {
+    it('should remove 1', () => {
+      service.add();
+      service.add();
+      service.remove();
+      service.counter$.subscribe((value) => expect(value).toEqual(1));
+    });
+  });
 });
