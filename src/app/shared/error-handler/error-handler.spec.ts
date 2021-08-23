5e8d5e3290a5889bd665fa02a07dfadace1a3db6
Andy Wright <atwright147@gmail.com>
2021-08-23 17:44:36 +0100

    add tests for the custom error handler

--- src/app/shared/error-handler/error-handler.spec.ts
+++ src/app/shared/error-handler/error-handler.spec.ts
@@ -1,7 +1,23 @@
 import { CustomErrorHandler } from './error-handler';
 
 describe('CustomErrorHandler', () => {
+  let klass: CustomErrorHandler;
+
+  beforeEach(() => {
+    klass = new CustomErrorHandler();
+  });
+
   it('should create an instance', () => {
     expect(new CustomErrorHandler()).toBeTruthy();
+  });
+
+  it('should log the error to the console', () => {
+    const mockError = { message: 'Mock error message' };
+    const consoleInfoSpy = spyOn(console, 'info');
+
+    klass.handleError(mockError);
+
+    expect(consoleInfoSpy).toHaveBeenCalledTimes(1);
+    expect(consoleInfoSpy).toHaveBeenCalledWith(mockError);
   });
 });
